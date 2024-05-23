e6c0e6745ae4d83b28171e6a427a5d840a3f7f17
igordmouraa <igomora04@gmail.com>
2024-05-23 11:27:39 -0300

    feat: Create website config

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+// config/site.ts
+
+export const siteConfig = {
+    name: "Igor Moura", // Nome do seu site
+    links: {
+      github: "https://github.com/igordmouraa", // Link para o seu GitHub
+      twitter: "https://twitter.com/seu-usuario", // Link para o seu Twitter
+      // Adicione outros links relevantes aqui
+    },
+    // Outras informações do site (e.g., endereço, telefone, etc.)
+  };
+  
