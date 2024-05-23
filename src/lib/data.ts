6372fd2fac9046732f9ea589c5bd2c90d9554d94
igordmouraa <igomora04@gmail.com>
2024-05-23 11:16:22 -0300

    feat: Create website data

---
+++ src/lib/data.ts
@@ -0,0 +1,41 @@
+// lib/data.ts
+
+import { Github, Twitter, Linkedin } from "lucide-react";
+
+export const socialMedia = [
+  {
+    name: "GitHub",
+    href: "https://github.com/igordmouraa",
+    icon: Github,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/igord_moura",
+    icon: Twitter,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/igordmoura/",
+    icon: Linkedin,
+  },
+];
+
+export const features = [
+  {
+    name: "Rápido e Eficiente",
+    description:
+      "Construído com tecnologias modernas para garantir o melhor desempenho.",
+    icon: "FastForward", // Ícone Lucide
+  },
+  {
+    name: "Fácil de Usar",
+    description: "Interface intuitiva e componentes personalizáveis.",
+    icon: "User", // Ícone Lucide
+  },
+  {
+    name: "Design Moderno",
+    description:
+      "Estilo elegante e minimalista que se adapta a qualquer projeto.",
+    icon: "LayoutDashboard", // Ícone Lucide
+  },
+];
